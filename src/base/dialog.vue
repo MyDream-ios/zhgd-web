132eda7a5877866d226dc54564af730e8205aba2
stark <784184859@qq.com>
2019-10-24 13:07:44 +0800

    已修改        src/utils/axios.js

---
+++ src/base/dialog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="dialog-warp">
+      <div class="header">{{}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
